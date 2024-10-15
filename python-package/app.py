53750df484ab6beb1e5df39d06fad68f87adc023
chris48s <git@chris-shaw.dev>
2024-10-15 15:00:57 +0100

    fix ruff formatting errors

--- python-package/app.py
+++ python-package/app.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-class DesignSystemConfig(AppConfig): # Our app config class
-    name = 'dc_design_system'
+
+class DesignSystemConfig(AppConfig):  # Our app config class
+    name = "dc_design_system"
     verbose_name = "Democracy Club Design System"
